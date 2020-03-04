410b30ce13506af6150c343d7e5ba006c818a225
山丘 <gengze@innotechx.com>
2020-03-05 00:38:58 +0800

    fix: use npm.cmd on windows

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,9 @@ const NODE_DIST = "https://npm.taobao.org/mirrors/node";
 const home = process.env.HOME;
 
 const restArgv = process.argv.slice(2);
-spawn('npm', [
+const isWin = process.platform === 'win32';
+const npmCmd = isWin ? 'npm.cmd' : 'npm';
+spawn(npmCmd, [
   `--registry=${NPM_REGISTRY}`,
   `--disturl=${NODE_DIST}`,
   `--userconfig=${home}/.inpmrc`,
